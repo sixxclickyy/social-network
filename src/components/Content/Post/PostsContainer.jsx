6b07b5a8a799c9b7fcc0535d2020a69a7bc085fc
Natalia <kostolomova6@gmail.com>
2023-05-10 23:02:59 +0300

    new commit

--- src/components/Content/Post/PostsContainer.jsx
+++ src/components/Content/Post/PostsContainer.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { addPosatActionCreator, onChangeActionCreator } from '../../../redux/contentReduser';
-import Posts from "./Posts";
+// import React from "react";
+// import { addPosatActionCreator, onChangeActionCreator } from '../../../redux/contentReduser';
+// import Posts from "./Posts";
 
-const PostsContainer = (props) => {
-  let AddPost = () => { 
-    props.store.dispatch(addPosatActionCreator());
-  };
+// const PostsContainer = (props) => {
+//   let AddPost = () => { 
+//     props.store.dispatch(addPosatActionCreator());
+//   };
 
-  let onChange = (text) => {
-    let action = onChangeActionCreator(text);
-    props.store.dispatch(action);
-  }
-  return (
-    <Posts ChangeInputText={onChange} addPost={AddPost} posts={props.posts}/>
-  );
-};
+//   let onChange = (text) => {
+//     let action = onChangeActionCreator(text);
+//     props.store.dispatch(action);
+//   }
+//   return (
+//     <Posts ChangeInputText={onChange} addPost={AddPost} posts={props.posts}/>
+//   );
+// };
 
-export default PostsContainer;
+// export default PostsContainer;
