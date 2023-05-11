bc3c3ecc28f15c52624282b861ed71f2408fc7d8
Natalia <kostolomova6@gmail.com>
2023-05-12 00:44:45 +0300

    тут я пофиксила баг со state ура

--- src/index.js
+++ src/index.js
@@ -26,3 +26,4 @@ store.subscribe(() => {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//
